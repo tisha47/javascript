9404a0983252cd800ed43c9192398a65eb2c4ae6
tisha47 <tishajadavd.47@gmail.com>
2025-01-10 11:06:41 +0530

    if else else if statement ex

--- home.js
+++ home.js
@@ -17,15 +17,28 @@
 // }
 // console.log(c);
 
-function reversNumber(num){
-    if (num>=0 && num<=9){
-        return num;
-    }
-    let reversNumber = 0;
-     while(num != 0){
-        reversNumber = reversNumber * 10 + (num % 10);
-        num = Math.floor(num / 10);
-     }
-     return reversNumber;
+// function reversNumber(num){
+//     if (num>=0 && num<=9){
+//         return num;
+//     }
+//     let reversNumber = 0;
+//      while(num != 0){
+//         reversNumber = reversNumber * 10 + (num % 10);
+//         num = Math.floor(num / 10);
+//      }
+//      return reversNumber;
+// }
+// console.log(reversNumber(321));
+
+let a = prompt("what's your age ?");
+a = Number.parseInt(a);
+if (a <=0){
+    alert("this is not valid");
+}else if (a>0 && a<=9){
+    alert("you are a kid you can not drive");
+}else if(a>=9 && a<18){
+    alert("you can thing for drive after 18");
+}else{
+    alert("you can driver");
 }
-console.log(reversNumber(321));
+console.log(a);
